b1cf409133664d9fafad3f432cf15f3c351cf1c6
MagdaMB <magda.m.badura@gmail.com>
2021-04-21 21:13:58 +0200

    fix - menu and validation form

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 const mobileMenu = document.querySelector(".hamburger__box");
 const menuList = document.querySelector(".header__menu.desktop");
-mobileMenu.addEventListener('click', function() {
+mobileMenu.addEventListener('click', function () {
     menuList.classList.toggle("visible");
 });
 
-// const form = document.getElementById("form");
-// const firstName = document.querySelector(".name");
-// const surname = document.querySelector(".surname");
-// const phone = document.querySelector(".phone");
-// const errorMessage = document.querySelector(".validationErrorMessage");
+let form = document.getElementById("form");
+let firstName = document.querySelector(".name");
+let surname = document.querySelector(".surname");
+let phone = document.querySelector(".phone");
+let errorMessage = document.querySelector(".validationErrorMessage");
 
-// form.addEventListener("submit", function(e) {
-//     e.preventDefault();
-//     let arrErrorMessage =[];
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let arrErrorMessage =[];
 
-//     if(firstName.value.length<=3) {
-//         arrErrorMessage.push("Podaj poprawne imię");
-//     }
+    if(firstName.value.length<3) {
+        arrErrorMessage.push("Podaj poprawne imię");
+    }
 
-//     if(surname.value.length<=3) {
-//         arrErrorMessage.push("Podaj poprawne nazwisko");
-//     }
+    if(surname.value.length<3) {
+        arrErrorMessage.push("Podaj poprawne nazwisko");
+    }
 
-//     const reg = /^(\d{9})$/;
-//     if(!reg.test(phone.value)) {
-//         arrErrorMessage.push("Podaj poprawny nr telefonu");
-//     }
+    let reg = /^(\d{9})$/;
+    if(!reg.test(phone.value)) {
+        arrErrorMessage.push("Podaj poprawny nr telefonu");
+    }
 
-//     if(!arrErrorMessage.length) {
-//         e.target.submit();
-//     } else {
-//         arrErrorMessage.innerHTML = `<p>Występują błędy w formularzu</p>`;
-//     }
-// });
+    if(!arrErrorMessage.length) {
+        e.target.submit();
+    } else {
+        errorMessage.innerHTML = `<ul>${arrErrorMessage.map(elem => `<li>${elem}</li>`).join("")}</ul>`;
+    }
+});
+
+let liItems = document.querySelectorAll(".menu__items a");
+
+liItems.forEach(aLink => {
+    aLink.addEventListener('click', function() {
+        liItems.forEach(elem => elem.classList.remove("active"));
+        this.classList.add("active");
+    })
+})
